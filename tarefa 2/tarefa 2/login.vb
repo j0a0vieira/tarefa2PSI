7040dde6639c3ffef2ef5700c3c8568cfded1ff0
vieiraCoder <56136565+vieiraCoder@users.noreply.github.com>
2020-04-17 11:40:58 +0100

    Add files via upload

---
+++ tarefa 2/tarefa 2/login.vb
@@ -0,0 +1,14 @@
+﻿Public Class login
+    Private Sub login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If user.Text = "admin" And pass.Text = "admin" Then
+            Form1.Show()
+            Me.Hide()
+        Else
+            user.Clear()
+            pass.Clear()
+        End If
+    End Sub
+End Class
